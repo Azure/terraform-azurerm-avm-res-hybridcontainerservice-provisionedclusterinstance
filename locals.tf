30b5ffeaeca08349880b360e312a766b62892378
Hangyu Xu <hangxu@microsoft.com>
2024-10-10 12:05:31 +0800

    update

--- locals.tf
+++ locals.tf
@@ -3,10 +3,10 @@ locals {
     for k, v in pool : k => (k == "nodeTaints" ? flatten(v) : v) if v != null
   }]
   os_sku = var.agent_pool_profiles[0].osSKU
-  # Split the resource group ID into parts based on '/'
-  parts = split("/", var.resource_group_id)
   # The resource group name is the last element of the split result
-  resource_group_name                = element(local.parts, length(local.parts) - 1)
+  resource_group_name = element(local.resource_group_parts, length(local.resource_group_parts) - 1)
+  # Split the resource group ID into parts based on '/'
+  resource_group_parts               = split("/", var.resource_group_id)
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
   ssh_public_key                     = var.ssh_public_key == null ? tls_private_key.rsa_key[0].public_key_openssh : var.ssh_public_key
 }
